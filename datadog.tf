9927b63862f2b472771717d86d61142e628404b9
roylines <roy@roylines.co.uk>
2016-04-15 09:05:27 +0100

    added datadog

---
+++ datadog.tf
@@ -0,0 +1,12 @@
+variable "datadog_api_key" {
+  description = "datadog api key"
+}
+
+module "datadog" {
+  //source = "git::https://github.com/roylines/terraform-ecs-datadog?ref=v1.0.0" 
+  source = "../terraform-ecs-datadog"
+  vpc = "${var.vpc}"
+  cluster_id = "${module.ecs.cluster_id}"
+  desired_count = "${var.cluster_desired_size}"
+  api_key = "${var.datadog_api_key}"
+}
