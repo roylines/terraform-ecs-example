bab7d0c55c2b1cf838a2def061c58a1182a29051
roylines <roy@roylines.co.uk>
2016-04-18 17:59:00 +0100

    support optional new relic

--- ecs.tf
+++ ecs.tf
@@ -1,6 +1,7 @@
 module "ecs" {
-  source = "git::https://github.com/roylines/terraform-ecs?ref=v2.1.1" 
+  source = "git::https://github.com/roylines/terraform-ecs?ref=v2.2.0" 
   vpc = "${var.vpc}"
   ssh_public_key = "${var.ssh_public_key}"
   cluster_desired_size = "${var.cluster_desired_size}"
+  newrelic_license_key = "${var.newrelic_license_key}"
 }
