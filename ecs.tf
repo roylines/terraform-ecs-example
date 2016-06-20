d830b2ed7a3f2ce92252ef291a1faafda15f73eb
roylines <roy@roylines.co.uk>
2016-06-20 09:48:15 +0100

    added ruxit and sysdig

--- ecs.tf
+++ ecs.tf
@@ -1,7 +1,10 @@
 module "ecs" {
-  source = "git::https://github.com/roylines/terraform-ecs?ref=v2.2.0" 
+  //source = "git::https://github.com/roylines/terraform-ecs?ref=v2.2.0" 
+  source = "../terraform-ecs" 
   vpc = "${var.vpc}"
   ssh_public_key = "${var.ssh_public_key}"
   cluster_desired_size = "${var.cluster_desired_size}"
   newrelic_license_key = "${var.newrelic_license_key}"
+  ruxit_account = "${var.ruxit_account}"
+  ruxit_token = "${var.ruxit_token}"
 }
